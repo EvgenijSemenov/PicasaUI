4c33de5ac0c6b56d762f1b215b34c7dcd2e1b589
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-07-26 09:02:21 +0300

    Add GoogleLoginComponent in declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
+import { GoogleLoginComponent } from './google-login/google-login.component';
+  
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoogleLoginComponent
   ],
   imports: [
     BrowserModule,
