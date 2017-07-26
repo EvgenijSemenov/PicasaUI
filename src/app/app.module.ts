291ed1955126f589afa8ed10512e367b71f59391
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-07-26 10:38:31 +0300

    Add RouterModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { GoogleLoginComponent } from './google-login/google-login.component';
+import { LoginComponent } from './login/login.component';
   
 @NgModule({
   declarations: [
     AppComponent,
-    GoogleLoginComponent
+    GoogleLoginComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: LoginComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
